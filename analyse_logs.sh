8d6d794cd466dc243088f25dd0ff5f94759236dc
kamransadiqali <you@example.com>
2025-08-31 17:18:33 +0500

    updates

--- analyse_logs.sh
+++ analyse_logs.sh
@@ -37,7 +37,7 @@ for LOG_FILE in $LOG_FILES; do
     echo -e "\n ⚠️ Action required: too many "$PATTERN" errors in log file "$LOG_FILE" "
  fi
     done 
-done
+done 
 
 echo -e "\nLog analysis completed and report saved in: $REPORT_FILE"
 
